2052388b4638e229ec2d6b02b080103cacc1d8c2
Aaron Hardy <aaron@aaronhardy.com>
2022-04-09 21:39:25 -0600

    Use useRef

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,21 +1,36 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const Counter = () => {
   console.log("render");
-  const [clickCount, setClickCount] = useState(0);
+  const timerIdRef = useRef(0);
+  const [count, setCount] = useState(0);
 
-  const incrementAfterOneSecond = () => {
-    setTimeout(() => {
-      setClickCount(clickCount => clickCount + 1);
+  const startHandler = () => {
+    if (timerIdRef.current) {
+      return;
+    }
+
+    timerIdRef.current = setInterval(() => {
+      setCount((latestCount) => latestCount + 1);
     }, 1000);
+  };
+
+  const stopHandler = () => {
+    clearInterval(timerIdRef.current);
+    timerIdRef.current = 0;
   };
 
   return (
     <div className="counter">
-      Click Count: {clickCount}
-      <button type="button" onClick={incrementAfterOneSecond}>Increment</button>
+      Timer: {count} seconds
+      <button type="button" onClick={startHandler}>
+        Start
+      </button>
+      <button type="button" onClick={stopHandler}>
+        Stop
+      </button>
     </div>
   );
-}
+};
 
 export default Counter;
