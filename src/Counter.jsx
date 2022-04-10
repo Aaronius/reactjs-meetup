0fb47118294b00ea22c206e5ecb684d48487c7e7
Aaron Hardy <aaron@aaronhardy.com>
2022-04-09 19:41:26 -0600

    Use useState.

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,11 +1,13 @@
+import { useState } from "react";
+
 const Counter = () => {
   console.log("render");
-  let clickCount = 0;
+  const [clickCount, setClickCount] = useState(0);
 
   return (
     <div className="counter">
       Click Count: {clickCount}
-      <button type="button" onClick={() => { clickCount++ }}>Increment</button>
+      <button type="button" onClick={() => { setClickCount(clickCount + 1); }}>Increment</button>
     </div>
   );
 }
