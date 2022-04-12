7abffb6869a639b0073408b398461ece54c1c389
Aaron Hardy <aaron@aaronhardy.com>
2022-04-12 17:31:45 -0600

    Focus input on button click

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import Counter from "./Counter";
-
-import './App.css'
+import {useRef} from "react";
+import "./App.css";
 
 function App() {
-  return <Counter/>;
+  const inputRef = useRef();
+
+  return (
+    <div className="app">
+      <input ref={inputRef} type="text" />
+      <button
+        onClick={() => {
+          inputRef.current.focus();
+        }}
+      >
+        Focus Input
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
